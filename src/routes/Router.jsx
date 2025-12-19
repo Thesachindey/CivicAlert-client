6c0c7276bdc2ecf7e67e1cd84723d98cfd63463d
Sachin <sachinscode24@gmail.com>
2025-12-19 10:41:32 +0600

    added dynamic route as for all role home page as a index-true

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,7 +4,6 @@ import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import AuthLayout from "../layout/AuthLayout";
-import NotFound from "../pages/NotFound/NotFoundPage";
 import NotFoundPage from "../pages/NotFound/NotFoundPage";
 import DashboardLayout from "../layout/DashboardLayout";
 import UpdateInfo from "../pages/Profile/UpdateInfo";
@@ -14,6 +13,21 @@ import AllIssues from "../pages/issues/AllIssues";
 import AboutUs from "../pages/AboutUs/AboutUs";
 import InsertIssue from "../pages/Dashboard/Citizen/InsertIssue";
 import IssueDetails from "../pages/issues/IssueDetails";
+import PaymentSuccess from "../pages/Dashboard/Payment/PaymentSuccess";
+import PaymentCanceled from "../pages/Dashboard/Payment/PaymentCanceled";
+import UpdateIssue from "../pages/Dashboard/Citizen/UpdateIssue";
+import MyIssues from "../pages/Dashboard/Citizen/MyIssues";
+import AdminAllIssues from "../pages/Dashboard/Admin/AdminAllIssues";
+import AdminRoute from "./AdminRoute";
+import AdminHome from "../pages/Dashboard/Admin/AdminHome";
+import ManageStaff from "../pages/Dashboard/Admin/ManageStaff";
+import PaymentHistory from "../pages/Dashboard/Admin/PaymentHistory";
+import StaffHome from "../pages/Dashboard/Staff/StaffHome";
+import StaffRoute from "./StaffRoute";
+import AssignedIssues from "../pages/Dashboard/Staff/AssignedIssues";
+import CitizenHome from "../pages/Dashboard/Citizen/CitizenHome";
+import ManageUsers from "../pages/Dashboard/Admin/ManageUsers";
+import DashboardHome from "../pages/Dashboard/DashboardHome";
 
 export const router = createBrowserRouter([
   {
@@ -24,38 +38,38 @@ export const router = createBrowserRouter([
         index: true,
         Component: Home
       },
-
       {
         path: '/about-us',
         element: <AboutUs />
-
       },
       {
         path: '/all-issues',
         element: <AllIssues />
-
       },
       {
         path: '/issue-details/:id',
-        element: <IssueDetails />
-
-      },
-      {
-        path: '/my-profile',
-        element:
+        element: (
           <PrivateRoute>
-            <MyProfilePage />
-          </PrivateRoute>,
-
+            <IssueDetails />
+          </PrivateRoute>
+        )
       },
       {
         path: '/auth/update-profile',
-        element:
+        element: (
           <PrivateRoute>
             <UpdateInfo />
           </PrivateRoute>
+        )
+      },
+      {
+        path: '/payment-success',
+        element: <PaymentSuccess />
+      },
+      {
+        path: '/payment-canceled',
+        element: <PaymentCanceled />
       }
-
     ]
   },
   {
@@ -63,10 +77,69 @@ export const router = createBrowserRouter([
     Component: DashboardLayout,
     children: [
       {
-        path: '/dashboard/report-issue',
-        element: <InsertIssue />
+        index: true,
+        element: <PrivateRoute><DashboardHome /></PrivateRoute>
       },
-
+      {
+        path: 'report-issue',
+        element: <PrivateRoute><InsertIssue /></PrivateRoute>
+      },
+      {
+        path: 'my-issues',
+        element: <PrivateRoute><MyIssues /></PrivateRoute>
+      },
+      {
+        path: 'edit-issue/:id',
+        element: (
+          <PrivateRoute>
+            <UpdateIssue />
+          </PrivateRoute>
+        )
+      },
+      {
+        path: 'citizen-home',
+        element: <PrivateRoute><CitizenHome /></PrivateRoute>
+      },
+      {
+        path: 'my-profile',
+        element: (
+          <PrivateRoute>
+            <MyProfilePage />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'manage-issues',
+        element: <AdminRoute><AdminAllIssues /></AdminRoute>
+      },
+      {
+        path: 'manage-staff',
+        element: <AdminRoute><ManageStaff /></AdminRoute>
+      },
+      {
+        path: 'manage-users',
+        element: <AdminRoute><ManageUsers /></AdminRoute>
+      },
+      {
+        path: 'payment-history',
+        element: <AdminRoute><PaymentHistory /></AdminRoute>
+      },
+      {
+        path: 'admin-home',
+        element: (
+          <AdminRoute>
+            <AdminHome />
+          </AdminRoute>
+        )
+      },
+      {
+        path: 'staff-home',
+        element: <StaffRoute><StaffHome /></StaffRoute>
+      },
+      {
+        path: 'assigned-issues',
+        element: <StaffRoute><AssignedIssues /></StaffRoute>
+      },
     ]
   },
   {
@@ -76,19 +149,15 @@ export const router = createBrowserRouter([
       {
         path: 'login',
         Component: Login,
-      }
-      ,
+      },
       {
         path: 'register',
         Component: Register,
       },
-
-
     ]
   },
   {
     path: '*',
     Component: NotFoundPage,
   }
-
 ]);
