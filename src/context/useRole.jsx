b711ccbbf34053d676620007f953188c9da0e50a
Sachin <sachinscode24@gmail.com>
2025-12-19 11:43:26 +0600

    added useRole component

---
+++ src/context/useRole.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import useAuth from '../Hooks/useAuth';
+import useAxiosSecure from '../Hooks/useAxiosSecure';
+
+const useRole = () => {
+    const { user, loading } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: role, isLoading: roleLoading } = useQuery({
+        queryKey: [user?.email, 'role'],
+        enabled: !loading && !!user?.email,
+        queryFn: async () => {
+
+            const res = await axiosSecure.get(`/users/${user.email}`);
+            return res.data?.role;
+        }
+    });
+
+    return { role, roleLoading };
+};
+
+export default useRole;
